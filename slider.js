6fdff85199a7042fbcfcbc7834cda5e2df22359d
prawnnastia <anastasia.sorokoumowa@gmail.com>
2021-06-14 15:09:55 +0300

    Add font, slider, array

--- slider.js
+++ slider.js
@@ -12,3 +12,22 @@ function setupPrevNextBtns (prevBtn, nextBtn) {
 }
 
 setupPrevNextBtns(prevBtn, nextBtn);
+
+var imgArray = [];
+for (var i = 0; i < 27; i++) {
+    var imgNumber = i + 1 >= 10 ? i + 1 : '0' + (i + 1); 
+    var imgPath = 'data/lack.array/lack' + imgNumber + '.png'
+    imgArray.push(imgPath)
+}
+
+
+var slidesEl = document.querySelector('.embla__slide')
+imgArray.forEach(function(imgPath) {
+    var counter = 0
+    var imgEl = document.createElement ('img')
+    imgEl.style.width = '100%';
+    imgEl.src = imgPath;
+    slidesEl.appendChild(imgEl)
+})
+
+
