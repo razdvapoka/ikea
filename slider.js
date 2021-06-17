47198f7006620f36e7374b1c6e3be9c54dae8aec
prawnnastia <anastasia.sorokoumowa@gmail.com>
2021-06-17 18:52:02 +0300

    Add content

--- slider.js
+++ slider.js
@@ -1,3 +1,23 @@
+var imgArray = [];
+for (var i = 0; i < 19; i++) {
+    var imgNumber = i + 1 >= 10 ? i + 1 : '0' + (i + 1); 
+    var imgPath = 'data/lack-1/lack ' + imgNumber + '.png'
+    imgArray.push(imgPath)
+}
+
+var slidesEl = document.querySelector('.embla__container');
+imgArray.forEach(function(imgPath) {
+    var counter = 0;
+    var slideEl = document.createElement("div");
+    slideEl.classList.add('embla__slide');
+    var imgEl = document.createElement ('img');
+    imgEl.style.width = '100%';
+    imgEl.src = imgPath;
+
+    slideEl.appendChild(imgEl)
+    slidesEl.appendChild(slideEl);
+})
+
 var emblaNode = document.querySelector('.embla')
 var options = { loop: 1, speed: 100 }
 
@@ -12,22 +32,3 @@ function setupPrevNextBtns (prevBtn, nextBtn) {
 }
 
 setupPrevNextBtns(prevBtn, nextBtn);
-
-var imgArray = [];
-for (var i = 0; i < 27; i++) {
-    var imgNumber = i + 1 >= 10 ? i + 1 : '0' + (i + 1); 
-    var imgPath = 'data/lack.array/lack' + imgNumber + '.png'
-    imgArray.push(imgPath)
-}
-
-
-var slidesEl = document.querySelector('.embla__slide')
-imgArray.forEach(function(imgPath) {
-    var counter = 0
-    var imgEl = document.createElement ('img')
-    imgEl.style.width = '100%';
-    imgEl.src = imgPath;
-    slidesEl.appendChild(imgEl)
-})
-
-
